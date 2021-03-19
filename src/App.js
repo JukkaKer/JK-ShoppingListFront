47f2be9a1db2d3cba2c6cfe8e5b6b857ec5361c4
Jukka Keränen <n0keju00@students.oamk.fi>
2021-03-19 23:17:53 +0200

    Tehtävän luovutus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+
+const URL = 'http://localhost/shoppinglist/';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [desc, setDesc] = useState('');
+  const [amount, setAmount] = useState('');
+
+  useEffect(()=> {
+    fetch(URL + 'index.php')
+    .then(response => response.json())
+    .then(
+      (response) => {
+        setItems(response);
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }, []);
+
+  function save(e) {
+    e.preventDefault();
+    let status = 0;
+    fetch(URL+'add.php', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json', 
+      },
+      body: JSON.stringify({
+        desc: desc,
+        amount: amount
+      })
+    })
+    .then(res => {
+      status = parseInt(res.status);
+      return res.json();
+    })
+    .then(
+      (res) => {
+        if(status == 200) {
+          setItems(items => [...items, res]);
+          setDesc('');
+          setAmount('');
+        } else {
+          alert(res.error);
+        }
+      }, (error) => {
+        alert(error);
+      }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Shopping List</h3>
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={desc} onChange={e => setDesc(e.target.value)} type='text' placeholder='type description' required/>
+        <input value={amount} onChange={e => setAmount(e.target.value)} type='text' placeholder='type amount' required/>
+        <button>Add</button>
+      </form>
+      
+
+      <div>
+        {items.map(item => (
+          <p className="item" key={item.id}>
+            <span>{item.description}</span>
+            <span>{item.amount}</span>
+            <a href='#'>Delete</a>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
